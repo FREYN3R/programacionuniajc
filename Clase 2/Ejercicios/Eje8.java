2377923d01fa22aad249eb3c2af014cea9d874be
FREYN3R <79862895+FREYN3R@users.noreply.github.com>
2025-02-16 16:03:27 +0000

    Añadir ejercicio para encontrar el número mayor y menor en un arreglo de 30 números

---
+++ Clase 2/Ejercicios/Eje8.java
@@ -0,0 +1,54 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class Eje8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Arreglo para almacenar los 30 números
+        int[] numeros = new int[30];
+        
+        // Solicitar los 30 números
+        System.out.println("Ingrese 30 números:");
+        for (int i = 0; i < 30; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+        
+        // Encontrar el número mayor y menor
+        int mayor = numeros[0];
+        int menor = numeros[0];
+        
+        for (int num : numeros) {
+            if (num > mayor) {
+                mayor = num;
+            }
+            if (num < menor) {
+                menor = num;
+            }
+        }
+        
+        // Contar las repeticiones del mayor y menor
+        int repeticionesMayor = 0;
+        int repeticionesMenor = 0;
+        
+        for (int num : numeros) {
+            if (num == mayor) {
+                repeticionesMayor++;
+            }
+            if (num == menor) {
+                repeticionesMenor++;
+            }
+        }
+        
+        // Mostrar resultados
+        System.out.println("\nEl número mayor es: " + mayor);
+        System.out.println("Se repite " + repeticionesMayor + " veces.");
+        
+        System.out.println("\nEl número menor es: " + menor);
+        System.out.println("Se repite " + repeticionesMenor + " veces.");
+        
+        scanner.close();
+    }
+}
