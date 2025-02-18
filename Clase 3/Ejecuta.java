22a2c23506b2c52caf051efa9e09625f9f4be111
FREYN3R <79862895+FREYN3R@users.noreply.github.com>
2025-02-18 16:31:23 +0000

    Renombrar método para calcular sueldos y añadir clase de ejecución para pruebas

---
+++ Clase 3/Ejecuta.java
@@ -0,0 +1,11 @@
+public class Ejecuta {
+    public static void main(String[] args) {
+        // Crear el objeto para acceder a la clase MetodosArreglos
+        MetodosArreglos obj = new MetodosArreglos();
+
+        String[] nombres = { "Alan", "Devora", "Miguel", "Sandra", "Diego" };
+        int[] sueldos = { 100, 200, 150, 300, 100 };
+
+        System.out.println(obj.nombresSueldos(nombres, sueldos));
+    }
+}
--- Clase 3/MetodosArreglos.java
+++ Clase 3/MetodosArreglos.java
@@ -1,29 +1,27 @@
 public class MetodosArreglos {
-    
-    public String nombreSueldos (String[]n, int[]s) {
-            
-        int suma = 0;
 
-        //Calcular la suma de los sueldos de los empleados
+    public String nombresSueldos(String[] n, int[] s) {
+        int suma = 0;
+        // Calcular la suma de los sueldos de los empleados
         for (int i = 0; i < s.length; i++) {
             suma += s[i];
         }
 
-        //Calcular la promedio de los sueldos
+        // Calcular el promedio de los sueldos
         double promedio = suma / s.length;
 
         String cad = "";
-        int cont = 0;
+        int contador = 0;
 
         for (int i = 0; i < s.length; i++) {
             if (s[i] >= promedio) {
                 cad += n[i] + "\n";
-                cont++;
+                contador++;
             }
         }
 
-        cad += "Fueron " + cont + " Los empleados que tienen sueldo por encima del promedio: " +promedio;
+        cad += "Fueron " + contador + " los empleados que tienen sueldo por encima del promedio: " + promedio;
 
-        return cad; 
+        return cad;
     }
 }
