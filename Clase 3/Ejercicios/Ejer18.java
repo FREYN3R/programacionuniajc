bcbf6fe96e3728edfc8b629f458d530825336cb5
FREYN3R <79862895+FREYN3R@users.noreply.github.com>
2025-02-22 02:42:12 +0000

    Añadir método para generar y mostrar la tabla de multiplicar del 10

--- Clase 3/Ejercicios/Ejer18.java
+++ Clase 3/Ejercicios/Ejer18.java
@@ -1,5 +1,12 @@
 package Ejercicios;
 
 public class Ejer18 {
-    
-}
+
+        public static void main(String[] args) {
+            // Generar y mostrar la tabla de multiplicar del 10
+            int i = 10;
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%d x %d = %d\n", i, j, i * j);
+            }
+        }
+    }
