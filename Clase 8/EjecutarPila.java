9eaa9c0939118729093ce1c629489c6b580099f4
FREYN3R <79862895+FREYN3R@users.noreply.github.com>
2025-03-25 16:07:33 +0000

    Agregar clase EjecutarPila para demostrar el uso de la estructura Stack

---
+++ Clase 8/EjecutarPila.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class EjecutarPila {
+
+    public static void main(String[] args) {
+        
+        //Creacion de la estructura Stack(Pila)
+        Stack<String> nombres = new Stack<>();
+
+        System.out.println(nombres.empty()); 
+
+        //Insertar elementos en la pila
+        nombres.push("Pedro");
+        nombres.push("Juan");
+        nombres.push("Maria");
+        nombres.push("Camila");
+        nombres.push("Daniel");
+
+        //Imprimir la pila
+        System.out.println(nombres); //[Pedro, Juan, Maria, Camila, Daniel]
+
+        //Mostrar quien esta en el tope de la fila sin removerlo
+        System.out.println(nombres.peek()); //Daniel
+
+        //Buscar un elemento dentro de la pila
+        System.out.println(nombres.search("Camila")); // 2 --> 2-1 = 1
+
+        //Retirar un elemento de la pila
+        System.out.println(nombres.pop());
+
+        //Imprimir la pila retirada
+        System.out.println(nombres); //[Pedro, Juan, Maria, Camila]
+
+        //Verificar el tamaño de la pila
+        System.out.println(nombres.size()); //4
+
+    }
+
+}
